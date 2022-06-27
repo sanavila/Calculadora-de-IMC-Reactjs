a336ef8425c765b08cf0ebe0eb34a6bdb2c82731
sanavila <rute.leone@hotmail.com>
2022-06-27 17:34:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
+
+function App() {
+  
+  return (
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150}/>
+        </div>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          ...
+        </div>
+        <div className={styles.rightSide}>
+          ...
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
